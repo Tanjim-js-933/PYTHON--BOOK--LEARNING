42e3a7e761934472ed40ed44191fdc9ccad96354
Tanjim Ahammed <76731530+Tanjim-js-933@users.noreply.github.com>
2022-02-20 17:58:36 +0600

    ALL COMMITS

---
+++ CH--5---PROGRAMS.PY/TURTLE.PY
@@ -0,0 +1,5 @@
+import turtle
+
+turtle.forward(100)
+
+turtle.exitonclick()
---
+++ CH--6---PROGRAMS/NESTED--LOOP/SAARC.PY
@@ -0,0 +1,3 @@
+saarc = ["Bangladesh", "Afghanistan", "Bhutan", "Nepal", "India", "Pakistan", "Sri Lanka"]
+for country in saarc:
+    print(country, "is a member of SAARC")
